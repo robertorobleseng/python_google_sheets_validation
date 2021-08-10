453dff80a02ed6dd3277df9d30e526bb2da91fc9
Roberto Robles <rob2thinking@gmail.com>
2021-08-10 15:28:16 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,93 @@
+#####################################################################################
+#                                                                                   #
+# Overview                                                                          #
+#                                                                                   #
+#    google_sheets_validate.py --worksheet="daily_stock_summary"                    #
+#                                                                                   #
+# Description                                                                       #
+#                                                                                   #
+#    This script is used to validate the attributes of fields                       #
+#    in a google sheets database.                                                   #
+#                                                                                   #
+# Implementation                                                                    #
+#                                                                                   #
+#    date        07-04-2021                                                         #
+#    version     1.0.0                                                              #
+#                                                                                   #
+#####################################################################################
+
+#####################
+# Imports           #
+#####################
+
+import argparse
+import datetime
+import gspread
+import logging
+
+#####################
+# Arguments         #
+#####################
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--worksheet', '-w', required=True, help='Google Worksheet')
+
+args = parser.parse_args()
+
+#####################
+# Logging           #
+#####################
+
+log_date = datetime.date.today()
+
+log_file = "google_sheets_validate_{}.log".format(log_date)
+
+logging.basicConfig(format="%(asctime)s - %(filename)s - %(levelname)s - %(message)s", level=logging.INFO, filename=log_file)
+
+#####################
+# Functions         #
+#####################
+
+def create_connection():
+    '''creates google sheets connection'''
+
+    gs_conn = gspread.service_account('credentials.json')
+
+    return gs_conn
+
+def get_worksheet_values(gs_conn, wks_name):
+    '''returns all values from a worksheets as a list of disctionaries'''
+
+    wks = gs_conn.open(wks_name).sheet1
+
+    list_of_dicts = wks.get_all_records()
+
+    return list_of_dicts
+
+#####################
+# Main              #
+#####################
+
+def main():
+
+    logging.info("Connecting to google sheets database.")
+
+    gs_conn   = create_connection()
+
+    wks_name  = args.worksheet
+
+    values    = get_worksheet_values(gs_conn, wks_name)
+
+    for V in values:
+
+        print(V)
+
+
+#####################
+# Start             #
+#####################
+
+if __name__ == '__main__':
+    main()
+    
