7185d64ca4af1314dd4214e3b6d878b75656ad90
Roberto Robles <rob2thinking@gmail.com>
2021-08-10 17:22:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 #                                                                                   #
 # Overview                                                                          #
 #                                                                                   #
-#    main.py --worksheet="daily_stock_summary"                                      #
+#    main.py --schema="stocks_schema.json "--worksheet="daily_stocks_summary"       #
 #                                                                                   #
 # Description                                                                       #
 #                                                                                   #
@@ -21,8 +21,10 @@
 #####################
 
 import argparse
+import collections
 import datetime
 import gspread
+import json
 import logging
 
 #####################
@@ -30,6 +32,8 @@ import logging
 #####################
 
 parser = argparse.ArgumentParser()
+
+parser.add_argument('--schema', '-s', required=True, help='Database Schema')
 
 parser.add_argument('--worksheet', '-w', required=True, help='Google Worksheet')
 
@@ -48,6 +52,22 @@ logging.basicConfig(format="%(asctime)s - %(filename)s - %(levelname)s - %(messa
 #####################
 # Functions         #
 #####################
+
+def import_schema(file_name):
+    '''imports database schema'''
+
+    data = None
+
+    try:
+        with open(file_name) as contents:
+
+            data = json.load(contents, object_pairs_hook=collections.OrderedDict)
+
+    except (IOError) as E:
+
+        print("Error: {}".format(E))
+
+    return data
 
 def create_connection():
     '''creates google sheets connection'''
@@ -71,6 +91,29 @@ def get_worksheet_values(gs_conn, wks_name):
 
     return values
 
+def check_int(field, value):
+    '''checks if the field is an integer'''
+
+    if isinstance(value, int):
+        pass
+
+    else:
+        value_error(field, value)
+
+def check_string(field, value, length):
+    '''checks if the field length exceeds the expected length of characters'''
+
+    if len(value) <= length:
+        pass
+
+    else:
+        value_error(field, value)
+        
+def value_error(field, value):
+    '''logs as error if a value does not match the field specifications as listed in the database schema'''
+
+    logging.error("invalid value detected for column {}: \'{}\'".format(field, value))
+    
 #####################
 # Main              #
 #####################
@@ -79,17 +122,25 @@ def main():
 
     logging.info("Connecting to google sheets database.")
 
+    schema    = import_schema(args.schema)
+    
     gs_conn   = create_connection()
 
     wks_name  = args.worksheet
 
     values    = get_worksheet_values(gs_conn, wks_name)
 
-    for V in values:
-
-        # add validation logic here
+    for table in schema:
         
-        print(V)
+        logging.info("Running checks for table {}".format(table))
+
+        columns = list(schema[table])
+        
+            for V in values:
+
+            # add validation logic here
+        
+                print(V)
 
 
 #####################
